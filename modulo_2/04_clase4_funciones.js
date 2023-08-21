b7e15f3b89a9fcdde12c22c7e009103c47558721
3dwintny <tecnotony8@gmail.com>
2023-08-21 17:17:37 -0600

    funciones basicas

---
+++ modulo_2/04_clase4_funciones.js
@@ -0,0 +1,12 @@
+var nombre = 'Edwin', edad = 24;
+// JavaScript es un lenguaje de programación que permite crear 
+// funciones debilmente tipado, y no espera que siempre esten
+// asignados las funciones.
+function imprimirEdad(n, e)
+    {
+        console.log(`Hola, me llamo ${n} y tengo ${e} años`)
+    }
+
+imprimirEdad(nombre, edad);
+imprimirEdad('Eder', 20);
+imprimirEdad('Candido', 22);
